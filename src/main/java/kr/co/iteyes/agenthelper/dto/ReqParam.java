aae5c4085dcbc7a71a00f3a0d991b9be6e1043e3
jacob <joohyukjung@gmail.com>
2023-01-30 14:36:36 +0900

    경북대학교 프로필 추가

--- src/main/java/kr/co/iteyes/agenthelper/dto/ReqParam.java
+++ src/main/java/kr/co/iteyes/agenthelper/dto/ReqParam.java
@@ -1,14 +1,11 @@
 package kr.co.iteyes.agenthelper.dto;
 
 import kr.co.iteyes.agenthelper.entity.enums.ReqType;
-import kr.co.iteyes.agenthelper.exception.ResourceNotValidException;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 @Data
 public class ReqParam implements Serializable {
@@ -25,26 +22,6 @@ public class ReqParam implements Serializable {
     private String endDate;
 
     private String rcptNo;
-
-//    public String getStartDate() {
-//        try {
-//            return new SimpleDateFormat("yyyy-MM-dd").format(new SimpleDateFormat("yyyyMMdd").parse(startDate));
-//        } catch (ParseException e) {
-//            throw new ResourceNotValidException(e.getMessage());
-//        } catch (NullPointerException e) {
-//            throw new ResourceNotValidException("must not be null: startDate");
-//        }
-//    }
-//
-//    public String getEndDate() {
-//        try {
-//            return new SimpleDateFormat("yyyy-MM-dd").format(new SimpleDateFormat("yyyyMMdd").parse(endDate));
-//        } catch (ParseException e) {
-//            throw new ResourceNotValidException(e.getMessage());
-//        } catch (NullPointerException e) {
-//            throw new ResourceNotValidException("must not be null: endDate");
-//        }
-//    }
 
     public String getRcptNo() {
         try {
